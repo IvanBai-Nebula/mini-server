25553a5d538a250934281ef0d0559e9fc8cb7563
Ivan <tilony@qq.com>
2024-09-17 16:21:28 +0800

    修改命名

--- mini_server/asgi.py
+++ mini_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mini_font_server project.
+ASGI config for mini_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_font_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_server.settings")
 
 application = get_asgi_application()
