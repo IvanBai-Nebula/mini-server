5f40828c12d6a070d7f9690c3d015125a5ab1eaa
Ivan <tilony@qq.com>
2024-09-17 16:27:40 +0800

    修改结构

---
+++ mini_admin_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiniAdminApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mini_admin_api"
--- mini_front_api/apps.py
+++ mini_front_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MiniAdminServerConfig(AppConfig):
+class MiniFrontApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "mini_admin_server"
+    name = "mini_front_api"
