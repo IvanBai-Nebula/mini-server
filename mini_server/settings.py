d9edc526abfc9493d675c8f0a2707d1f2f3daba5
Ivan <tilony@qq.com>
2024-09-19 23:56:22 +0800

    更新

--- mini_server/settings.py
+++ mini_server/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "corsheaders"
+    "corsheaders",
     "rest_framework",
     "rest_framework_jwt",
     "user.apps.UserConfig",
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
